3e55f3018c6c5edb99236889ff6267651b7d93f4
SobakaBekka <st69704@upce.cz>
2024-02-17 18:56:47 +0100

    Исправил ошибки не понимания что такое Snake case также переменные не могут начинаться с спец символов или цыфр, но если нужно, то перед цыфрой или спец смиволом нужно ставить нижнее подчеркивание

--- src/main/java/school/mjc/stage0/module3/task2/JavaVariableNames.java
+++ src/main/java/school/mjc/stage0/module3/task2/JavaVariableNames.java
@@ -2,13 +2,13 @@ package school.mjc.stage0.module3.task2;
 
 public class JavaVariableNames {
     public static void main(String[] args) {
-        int tomorrow = 0;
-        int money = 10;
-        int hundred = 100;
-        int back = 1000;
-        int underscore = 10000;
-        int sharp = 100000;
-        int star = 1000000;
-        int badExampleOfVarname = 1000000;
+        int _2morrow = 0;
+        int $money = 10;
+        int _1hundred = 100;
+        int _1back = 1000;
+        int _underscore = 10000;
+        int _1sharp = 100000;
+        int _1star = 1000000;
+        int _1badExampleOfVarName = 10000000; // Аналогично
     }
 }
--- src/main/java/school/mjc/stage0/module3/task2/NamingConvention.java
+++ src/main/java/school/mjc/stage0/module3/task2/NamingConvention.java
@@ -2,13 +2,15 @@ package school.mjc.stage0.module3.task2;
 
 public class NamingConvention {
     public static void main(String[] args) {
-        int variableOne = 10;
-        int variableTwo = 1;
-        int variableThree = 2;
-        final int VARIABLE_FOUR = 3;
-        final int VARIABLE_FIVE = 4;
-        int variableSix = 5;
-        int variableSeven = 6;
-        int variableEight = 7;
+        int variable_0 = 10;
+        int variable_1 = 1;
+        int variable_2 = 2;
+        int variable_3 = 3;
+        final int VARIABLE_4 = 4;
+        final int VARIABLE_5 = 5;
+        int variable_6 = 6;
+        int variable_7 = 7;
+        int variable_8 = 8;
     }
 }
+
